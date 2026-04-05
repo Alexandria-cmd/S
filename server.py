f2a475a718c1e516b63e2456719a0538746adf80
Alexandria <dianaink22@gmail.com>
2026-04-05 03:56:40 +0300

    Add files via upload

---
+++ server.py
@@ -0,0 +1,34 @@
+import socket
+import time
+from datetime import datetime
+
+def start_server():
+	try:
+		server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		server.bind(('127.0.0.1',2000))
+		server.listen(100)
+		while True:
+			print('Сервер запущен.')
+			client_socket, address = server.accept()
+			data = client.socket.recv(1024).decode('utf-8')
+			content = load_page_from_get_request(data)
+			client_socket.send(content)
+			client_socket.shutdown(socket.SHIT_WR)
+	except KeybordInterrupt:
+		server.close()
+		print('Сервер остановленн в ручную.')
+		
+def load_page_from_get_requests(request_data):
+	HDRS = 'HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=utf-8\r\n\r\n'
+	HDRS_404 = 'HTTP/1.1 404 OK\r\nContent-Type: text/html; charset=utf-8\r\n\r\n'
+	patf = requests_data.split(' ')[1]
+	respons = ''
+	try:
+		with open('views'+path, 'rb') as file:
+			response = file.read()
+		return HDRS.encode('utf-8') + response
+	except FileNotFoundError:
+		return (HDRS_404 + 'Данной странницы не существует.').encode('utf-8')
+		
+if __name__ == '__main__':
+	start_server()
